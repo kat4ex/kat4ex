d1413d65150b7d5088a7f146e87f4b1a6d47f454
Синицын Никита Владимирович <sinitsyn.nv@devzone.local>
2024-09-21 17:11:14 +0500

    Добавление пользователя в группу docker после установки
    Former-commit-id: ba99994d85fbba0e3f189543af713188465f21ac

--- get-docker.sh
+++ get-docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-apt-get update
-apt-get install curl -y
+sudo apt-get update
+sudo apt-get install curl -y
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 rm get-docker.sh
 docker --version
+
+if command -v usermod >/dev/null 2>&1; then
+  sudo usermod -aG docker $USER
+fi
